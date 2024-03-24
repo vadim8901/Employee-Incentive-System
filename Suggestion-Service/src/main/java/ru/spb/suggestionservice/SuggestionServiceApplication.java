1dc1f500be6fcfe633069f88bdafd1b33628b6df
gryaznykh.vs <gryaznykh.vs@esphere.ru>
2024-03-24 22:38:49 +0300

    init

---
+++ Suggestion-Service/src/main/java/ru/spb/suggestionservice/SuggestionServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.spb.suggestionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuggestionServiceApplication.class, args);
+    }
+
+}
---
+++ Suggestion-Service/src/test/java/ru/spb/suggestionservice/SuggestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spb.suggestionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
